32f6d5fe90cafb56acdbb9b0263575e34b1bea98
briannalturner <blturner3527@gmail.com>
2020-02-28 15:58:23 -0500

    New User Form Fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_many :user_talents
     has_many :talents, through: :user_talents
     belongs_to :city
-    belongs_to :house
+    belongs_to :house, optional: true
 
     validates :name, presence: true
+    validates :drag_name, presence: true, uniqueness: {case_sensitive: false}
 end
