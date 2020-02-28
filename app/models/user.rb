1e270b1da15800055030c79c5bc380a99441fdb7
matteoaricci <matteo.a.ricci@gmail.com>
2020-02-28 15:03:09 -0500

    Added some house pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :user_talents
     has_many :talents, through: :user_talents
+    belongs_to :house
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,13 @@ House.destroy_all
 Talent.destroy_all
 UserTalent.destroy_all
 
-matt = User.create(name: "Matteo Ricci", drag_name: "Auntie Depressant", city_id: 1, house_id: nil)
-bri = User.create(name: "Bri Turner", drag_name: "Amy Bipolar", city_id: 1, house_id: nil)
-carl = User.create(name: "Carl Parm", drag_name: "Lady Footlocker", city_id: 1, house_id: nil)
-paul = User.create(name: "Paul Kim", drag_name: "Mona Lott", city_id: 1, house_id: nil)
+ed = House.create(name: "Edwards", description: "Belongs to Miss Gay Justin", mother: "Alyssa Edwards")
+parm = House.create(name: "Parmesean", description: "Premo Italian Shit", mother: "Donatella Versace")
+
+matt = User.create(name: "Matteo Ricci", drag_name: "Auntie Depressant", city_id: 1, house_id: ed.id)
+bri = User.create(name: "Bri Turner", drag_name: "Amy Bipolar", city_id: 1, house_id: ed.id)
+carl = User.create(name: "Carl Parm", drag_name: "Lady Footlocker", city_id: 1, house_id: parm.id)
+paul = User.create(name: "Paul Kim", drag_name: "Mona Lott", city_id: 1, house_id: parm.id)
 will = User.create(name: "Will Harris", drag_name: "Avery Goodlay", city_id: 1, house_id: nil)
 yo = User.create(name: "Yo Park", drag_name: "Annie Mae", city_id: 1, house_id: nil)
 ryan = User.create(name: "Ryan Wilson", drag_name: "Toyotathot", city_id: 1, house_id: nil)
@@ -35,5 +38,3 @@ ut2 = UserTalent.create(user_id: bri.id, talent_id: makeup.id)
 ut3 = UserTalent.create(user_id: carl.id, talent_id: fashion.id)
 ut4 = UserTalent.create(user_id: will.id, talent_id: imperse.id)
 
-ed = House.create(name: "Edwards", description: "Belongs to Miss Gay Justin", mother: "Alyssa Edwards")
-parm = House.create(name: "Parmesean", description: "Premo Italian Shit", mother: "Donatella Versace")
