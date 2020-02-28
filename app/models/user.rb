0ffa7c4d4e4848ff165271d9b46cd731e4e1389d
matteoaricci <matteo.a.ricci@gmail.com>
2020-02-28 15:07:32 -0500

    just merged

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
     has_many :user_talents
     has_many :talents, through: :user_talents
-<<<<<<< HEAD
-=======
     belongs_to :city
->>>>>>> master
     belongs_to :house
 end
