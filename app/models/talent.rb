cce544aea7612917d9971618a73ea512b487a919
matteoaricci <matteo.a.ricci@gmail.com>
2020-02-28 14:12:56 -0500

    Talents shows queens w talent

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -1,2 +1,4 @@
 class Talent < ApplicationRecord
+    has_many :user_talents
+    has_many :users, through: :user_talents
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :user_talents
+    has_many :talents, through: :user_talents
 end
