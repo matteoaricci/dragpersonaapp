582d240445d95621d37fe0f1ccbead831200fc6b
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-02 11:49:51 -0500

    More changes before merge

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+
     describe "render pages" do
-    subject(:a) { get "/users/new" }
-    subject(:b) { get "/users"}
+
+        subject(:a) { get "/users/new" }
+        subject(:b) { get "/users"}
 
         it "renders the new page" do
             expect(a).to render_template(:new)
@@ -12,6 +14,7 @@ RSpec.describe "Users", type: :request do
         it "renders the detail page" do
             expect(b).to render_template(:index)
         end
-
     end
+
+    
 end
