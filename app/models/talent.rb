00b9c6e010621141072a040cdd7ab056482ab07e
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-02 17:33:20 -0500

    added method to Talent model

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -6,4 +6,18 @@ class Talent < ApplicationRecord
         self.all.max_by{ |talent| talent.users.count }
     end
 
+    def self.talents_with_users
+        user_talent_num = {}
+        User.all.each do |user|
+            if user.talents.any?
+                if user_talent_num[user.talents[0][:name]]
+                    user_talent_num[user.talents[0][:name]] += 1
+                else
+                    user_talent_num[user.talents[0][:name]] = 1
+                end
+            end
+        end
+        user_talent_num
+    end
+
 end
