9cad53b889d7c2f7a9a009718efe388fff163273
briannalturner <blturner3527@gmail.com>
2020-03-06 09:49:01 -0500

    inbox

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/home', to: 'application#homepage', as: 'homepage'
   get '/analytics', to: 'application#analytics', as: 'analytics'
+  get '/inbox', to: 'messages#inbox', as: 'inbox'
   get '/', to: 'sessions#new', as: 'login'
   post '/', to: 'sessions#create'
   get '/logout', to: 'sessions#logout', as: 'logout'
   get '/logoutyes', to: 'sessions#logout_yes', as: 'logouts'
   get '/messages/:id/reject', to: 'application#reject', as: 'reject'
   get '/messages/:id/accept', to: 'application#accept', as: 'accept'
+  get '/sent/:id', to: 'messages#sent_show', as: 'sent_show'
+  get '/sent', to: 'messages#sent', as: 'sent'
   resources :users
   resources :talents
   resources :houses
