68590a0c71b37f9181aa0b07c1605413c0348034
briannalturner <blturner3527@gmail.com>
2020-03-05 21:31:30 -0500

    users can send and recieve messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,39 @@
 class MessagesController < ApplicationController
 
+    before_action :find_message, only: [:show]
+
     def index
         @messages = Message.all.select { |msg| msg.recipient_id == current_user.id }
     end
 
+    def show
+        @sender = User.find_by(id: @message.sender_id)
+    end
+
+    def new
+        @message = Message.new
+    end
+
+    def create
+        @message = Message.new(message_params)
+        if @message.valid?
+            @message.sender_id = current_user.id
+            @message.save
+            redirect_to message_path(@message)
+        else
+            render new_message_path
+        end
+    end
+
+
+    private
+
+    def find_message
+        @message = Message.find(params[:id])
+    end
+
+    def message_params
+        params.require(:message).permit(:recipient_id, :subject, :body)
+    end
 
 end
