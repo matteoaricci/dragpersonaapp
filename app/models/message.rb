55f71c3285370ccfbb5be48dd43252a1883f6aaa
briannalturner <blturner3527@gmail.com>
2020-03-05 21:43:38 -0500

    validations for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+
+    validates :subject, :body, presence: true 
+    validates :subject, length: { maximum: 15 }
+    validates :body, length: {maximum: 75}
+
 end
